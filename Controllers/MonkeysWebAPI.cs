c0f849b8f482397a2c45ecfdff551660f1e70fad
User <User@LAB2-2>
2024-04-10 09:33:53 +0300

    gftuy

--- Controllers/MonkeysWebAPI.cs
+++ Controllers/MonkeysWebAPI.cs
@@ -7,30 +7,48 @@ using System.Collections.Generic;
 
 namespace CalculatorWebAPI.Controllers
 {
-    [Route("MonkeyApi")]
+    [Route("MonkeyAPI")]
     [ApiController]
     public class MonkeysWebAPI : ControllerBase
     {
-        [HttpGet("ReadAllMonkeys")]
-        public IActionResult ReadMonkeys()
+        [HttpGet("GetAllMonkeys")]
+        public IActionResult GetAllMonbkeys()
         {
-            MonkeyListDto list = new MonkeyListDto
-            {
-                Monkeys = new List<MonkeyDto>()
-            };
+            MonkeyListDto list = new MonkeyListDto();
+            list.Monkeys = new List<MonkeyDto>();
+
             MonkeyList monkeys = new MonkeyList();
-            foreach(Monkey m in monkeys.Monkeys)
+
+            foreach (Monkey m in monkeys.Monkeys)
             {
                 list.Monkeys.Add(new MonkeyDto()
                 {
                     Name = m.Name,
                     ImageUrl = m.ImageUrl,
                     Location = m.Location,
-                    IsFavorite = m.IsFavorite,
                     Details = m.Details
                 });
             }
             return Ok(list);
         }
+        [HttpGet("ReadMonkeys")]
+        public IActionResult ReadMonkey([FromQuery] string str)
+        {
+            MonkeyDto monkey = new MonkeyDto();
+            MonkeyList monkeys = new MonkeyList();
+            foreach (Monkey m in monkeys.Monkeys)
+            {
+                if (m.Name == str)
+                {
+                    monkey.Name = m.Name;
+                    monkey.ImageUrl = m.ImageUrl;
+                    monkey.Location = m.Location;
+                    monkey.Details = m.Details;
+                    return Ok(monkey);
+                }
+            }
+            return NotFound();
+        }
     }
+
 }
